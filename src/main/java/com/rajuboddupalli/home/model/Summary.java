8b050801537fd060866d31dcc80d18b124bcc57d
rajumca <rajugriet483@gmail.com>
2018-12-30 15:27:10 -0500

    initial commit

---
+++ src/main/java/com/rajuboddupalli/home/model/Summary.java
@@ -0,0 +1,33 @@
+package com.rajuboddupalli.home.model;
+
+import java.util.List;
+
+public class Summary {
+    private double total;
+    private double avergae;
+    private List<Year> years;
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public double getAvergae() {
+        return avergae;
+    }
+
+    public void setAvergae(double avergae) {
+        this.avergae = avergae;
+    }
+
+    public List<Year> getYears() {
+        return years;
+    }
+
+    public void setYears(List<Year> years) {
+        this.years = years;
+    }
+}
---
+++ src/main/java/com/rajuboddupalli/home/model/Year.java
@@ -0,0 +1,51 @@
+package com.rajuboddupalli.home.model;
+
+import java.util.List;
+
+public class Year {
+    private int year;
+    private double total;
+    private int months;
+    private double average;
+    private List<Transaction> transactions;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public void setMonths(int months) {
+        this.months = months;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    public void setAverage(double average) {
+        this.average = average;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+}
